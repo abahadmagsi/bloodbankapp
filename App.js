afd5d5a5d2458b36961857f8cd5ee5e58f7b8b33
Abdul Ahad Magsi <39968322+abahadmagsi@users.noreply.github.com>
2021-02-08 20:15:15 +0500

    'ready'

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "./src/screens/HomeScreen";
+import RegistrationScreen from "./src/screens/RegistrationScreen";
+import DonarsScreen from "./src/screens/DonarsScreen";
+import DonarProfile from "./src/screens/DonarProfile";
+const navigator = createStackNavigator(
+  {
+    Home: { screen: HomeScreen },
+    RegistrationScreen: { screen: RegistrationScreen },
+    DonarsScreen: { screen: DonarsScreen },
+    DonarProfile: { screen: DonarProfile },
   },
-});
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      title: "By Ahad",
+    },
+  }
+);
+export default createAppContainer(navigator);
